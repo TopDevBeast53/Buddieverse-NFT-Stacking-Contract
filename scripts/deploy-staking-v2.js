6877c4f6a761f50a546b1fe0a5763d7bc7760994
Pavlo <richardlexon87@gmail.com>
2023-04-18 17:53:06 +0300

    update deploy script

--- scripts/deploy-staking-v2.js
+++ scripts/deploy-staking-v2.js
@@ -1,5 +1,7 @@
 const SeedToken = require("../artifacts/contracts/SeedToken.sol/SeedToken.json");
 const NFTCollection = require("../artifacts/contracts/NFTCollection.sol/NFTCollection.json");
+const BudStaking = require("../artifacts/contracts/BudStaking.sol/BudStaking.json");
+const { BigNumber } = require("ethers");
 
 async function main() {
   // This is just a convenience check
@@ -20,29 +22,28 @@ async function main() {
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
+  const seedToken = new ethers.Contract("0xedEdF53A59625755De62074C2d222B1d9914d6f0", SeedToken.abi, deployer);
+  console.log("SeedToken address:", seedToken.address);
+
+  const collection = new ethers.Contract("0x96FCB2984F43f652E4430763a7e5Bb76146F5371", NFTCollection.abi, deployer);
+  console.log("NFTCollection address:", collection.address);
+
   const Staking = await ethers.getContractFactory("BudStaking");
   const staking = await Staking.deploy(collection.address, seedToken.address);
 	await staking.deployed();
   console.log("Staking contract address:", staking.address);
 
-  const seedToken = new ethers.Contract("0xedEdF53A59625755De62074C2d222B1d9914d6f0", SeedToken.abi, deployer);
-  console.log("SeedToken address:", seedToken.address);
-
-  const from = "0xAAf9E0613910916f55c0d648F26127b6901Ce471";
-  const balance = await seedToken.balanceOf(from);
-  console.log("Balance of seed tokens", balance)
-  
-  console.log("Send seed tokens to new staking contract");
-  await seedToken.transferFrom(from, staking.address, balance);
-  
-  const collection = new ethers.Contract("0x96FCB2984F43f652E4430763a7e5Bb76146F5371", NFTCollection.abi, deployer);
-  console.log("NFTCollection address:", collection.address);
+  console.log("Mint Seed token to address", staking.address);
+  const ethersToWei = ethers.utils.parseUnits("3000000", "ether");
+  await seedToken.mint(deployer.address, ethersToWei);
+  await seedToken.approve(staking.address, ethersToWei);
 
   console.log("Set approval for deployer", staking.address);
   await collection.setApprovalForAll(staking.address, true);
 
-  await collection.mint(deployer.address, 3);
-	await collection.mint(deployer.address, 4);
+  await collection.mint(deployer.address, 23);
+	await collection.mint(deployer.address, 24);
+	await collection.mint(deployer.address, 25);
 }
 
 main()
@@ -51,3 +52,7 @@ main()
     console.error(error);
     process.exit(1);
   });
+
+
+
+//npx hardhat run scripts/deploy-staking-v2.js --network bsc

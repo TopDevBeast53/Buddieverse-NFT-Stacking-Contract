5f127a712078cf53f4ed039c86b5255d739ed637
Pavlo <richardlexon87@gmail.com>
2023-04-20 09:27:06 +0300

    update deploy script

--- scripts/deploy-staking-v2.js
+++ scripts/deploy-staking-v2.js
@@ -3,6 +3,9 @@ const NFTCollection = require("../artifacts/contracts/NFTCollection.sol/NFTColle
 const BudStaking = require("../artifacts/contracts/BudStaking.sol/BudStaking.json");
 const { BigNumber } = require("ethers");
 
+const CONTRACT_SEED_TOKEN = "0xedEdF53A59625755De62074C2d222B1d9914d6f0";
+const CONTRACT_NFT_COLLECTION = "0x96FCB2984F43f652E4430763a7e5Bb76146F5371";
+
 async function main() {
   // This is just a convenience check
   if (network.name === "hardhat") {
@@ -22,10 +25,10 @@ async function main() {
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const seedToken = new ethers.Contract("0xedEdF53A59625755De62074C2d222B1d9914d6f0", SeedToken.abi, deployer);
+  const seedToken = new ethers.Contract(CONTRACT_SEED_TOKEN, SeedToken.abi, deployer);
   console.log("SeedToken address:", seedToken.address);
 
-  const collection = new ethers.Contract("0x96FCB2984F43f652E4430763a7e5Bb76146F5371", NFTCollection.abi, deployer);
+  const collection = new ethers.Contract(CONTRACT_NFT_COLLECTION, NFTCollection.abi, deployer);
   console.log("NFTCollection address:", collection.address);
 
   const Staking = await ethers.getContractFactory("BudStaking");
@@ -37,13 +40,6 @@ async function main() {
   const ethersToWei = ethers.utils.parseUnits("3000000", "ether");
   await seedToken.mint(deployer.address, ethersToWei);
   await seedToken.approve(staking.address, ethersToWei);
-
-  console.log("Set approval for deployer", staking.address);
-  await collection.setApprovalForAll(staking.address, true);
-
-  await collection.mint(deployer.address, 23);
-	await collection.mint(deployer.address, 24);
-	await collection.mint(deployer.address, 25);
 }
 
 main()

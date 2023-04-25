33f4806744d411e0f9723dd9394f73f3dce20696
Pavlo <richardlexon87@gmail.com>
2023-04-25 15:46:34 +0300

    fix: staked time

--- contracts/BudStaking.sol
+++ contracts/BudStaking.sol
@@ -313,19 +313,12 @@ contract BudStaking is Ownable, ReentrancyGuard, Pausable {
                     Staker memory staker = stakers[stakersArray[i]];
                     
                     durations[i] = 0;
-                    if (_updatedTime > 0) {
-                        uint256 elapsed = (endTime - _updatedTime) / SECONDS_IN_DAY;
+                    for (uint256 n; n < staker.stakedTokens.length; ++n) {
+                        uint256 stakedTime = Math.max(_updatedTime, staker.stakedTokens[n].timestamp);
+                        uint256 elapsed = (endTime - stakedTime) / SECONDS_IN_DAY;
                         if (elapsed > 0) {
-                            numOfTokens += staker.stakedTokens.length;
-                            durations[i] = elapsed * staker.stakedTokens.length;
-                        }
-                    } else {
-                        for (uint256 n; n < staker.stakedTokens.length; ++n) {
-                            uint256 elapsed = (endTime - staker.stakedTokens[n].timestamp) / SECONDS_IN_DAY;
-                            if (elapsed > 0) {
-                                numOfTokens += 1;
-                                durations[i] += elapsed;
-                            }
+                            numOfTokens += 1;
+                            durations[i] += elapsed;
                         }
                     }
                 }

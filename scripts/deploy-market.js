5bd911a8f2c52c60a296f36ed5f4d7df18062fbe
Pavlo <richardlexon87@gmail.com>
2023-05-12 10:21:58 +0300

    add script to deploy market

---
+++ scripts/deploy-market.js
@@ -0,0 +1,45 @@
+async function main() {
+  // This is just a convenience check
+  if (network.name === "hardhat") {
+    console.warn(
+      "You are trying to deploy a contract to the Hardhat Network, which" +
+        "gets automatically created and destroyed every time. Use the Hardhat" +
+        " option '--network localhost'"
+    );
+  }
+
+  // ethers is available in the global scope
+  const [deployer] = await ethers.getSigners();
+  console.log(
+    "Deploying the contracts with the account:",
+    await deployer.getAddress()
+  );
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const SeedToken = await ethers.getContractFactory("SeedToken");
+  const seedToken = await SeedToken.deploy(deployer.address, deployer.address);
+  await seedToken.deployed();
+  console.log("SeedToken address:", seedToken.address);
+
+  const Marketplace = await ethers.getContractFactory("Marketplace");
+  const marketplace = await Marketplace.deploy(seedToken.address);
+  await marketplace.deployed();
+  console.log("Marketplace address:", marketplace.address);
+
+  console.log("Mint Seed token to address", staking.address);
+  const ethersToWei = ethers.utils.parseUnits("3000000", "ether");
+  await seedToken.mint(staking.address, ethersToWei);
+
+  const balance = await seedToken.balanceOf(staking.address);
+  console.log("Balance of seed token", balance);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+//npx hardhat run scripts/deploy-staking-v3.js --network exosama
